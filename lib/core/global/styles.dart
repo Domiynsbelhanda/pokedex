70f0ce4859ee549d21103280d1cccadafacd4e9c
Youness Dominique <youness.dominique@gmail.com>
2023-03-09 18:31:57 +0200

    [FEAT] add styles to subtitlestyle

--- lib/core/global/styles.dart
+++ lib/core/global/styles.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SubtitleStyle extends TextStyle {
   final double size;
   final Color color;
   final FontWeight fontWeight;
+  final String fontFamily;
 
   const SubtitleStyle(
-      {this.size = 14,
-      this.color = Colors.grey,
-      this.fontWeight = FontWeight.normal})
+      {
+        this.size = 14,
+        this.color = Colors.grey,
+        this.fontWeight = FontWeight.normal,
+        this.fontFamily = 'Arial'
+      })
       : super(
           color: color,
           fontWeight: fontWeight,
           fontSize: size,
+          fontFamily: fontFamily
         );
 }
 
